5725bbe8aa8f2f9df2e049786f10c56ca94bd094
Sherif Ashraf <sherifashraf112233@gmail.com>
2019-11-04 11:18:47 +0200

    Update product.service.ts

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
-import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
-  private _albumUrl : '../assets/album.json'
+  private _albumUrl = '../assets/album.json'
   
   constructor(private _http: Http) { }
 
